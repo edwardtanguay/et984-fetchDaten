f77b0ec849a22d5e3802753d37edf34c70c3b98c
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-14 13:39:32 +0200

    search works again

--- src/components/GetWrapperHtml.js
+++ src/components/GetWrapperHtml.js
@@ -1,5 +1,20 @@
+import { GetCharacterListHtml } from "./GetCharacterListHtml";
+
 export const GetWrapperHtml = () => {
 	return /*html*/ `
 	<div class ="wrapper flex flex-wrap gap-4 justify-center items-center bg-slate-100 p-3 m-2"></div>
 	`;
 }; 
+
+export const attachWrapperEvents = (characters) => {
+	const input = document.querySelector(".input");
+	input.addEventListener("keyup", (e) => {
+		const keyword = e.target.value.toLowerCase();
+		if (keyword !== "") {
+			const filteredCharacters = characters.filter((char) => char.name.toLowerCase().includes(keyword));
+			document.querySelector(".wrapper").innerHTML = GetCharacterListHtml(filteredCharacters);
+			return;
+		}
+
+	});
+}
--- src/main.js
+++ src/main.js
@@ -2,22 +2,12 @@ import "./style.css";
 import { GetMainHtml } from "./components/GetMainHtml";
 import * as tools from './tools';
 import { GetCharacterListHtml } from './components/GetCharacterListHtml';
+import { attachWrapperEvents } from "./components/GetWrapperHtml";
+
 tools.fetchData((characters) => {
-
 	document.querySelector("#app").innerHTML = GetMainHtml();
-
 	document.querySelector(".wrapper").innerHTML = GetCharacterListHtml(characters);
 
-	const input = document.querySelector(".input");
-	input.addEventListener("keyup", (e) => {
-		const keyword = e.target.value.toLowerCase();
-		if (keyword !== "") {
-
-			const filteredCharacters = characters.filter((char) => char.name.toLowerCase().includes(keyword));
-			document.querySelector(".wrapper").innerHTML = GetCharacterListHtml(filteredCharacters);
-			return;
-		}
-
-	});
-
+	// attach events
+	attachWrapperEvents(characters);
 });
