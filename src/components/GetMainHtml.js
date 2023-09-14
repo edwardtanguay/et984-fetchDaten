4898c65aedde64f6b9451a04f7b2aec982fa4ca4
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-14 12:47:25 +0200

    getmainhtml

---
+++ src/components/GetMainHtml.js
@@ -0,0 +1,14 @@
+export const GetMainHtml = () => {
+	return /*html*/ `
+	<div class ="container">
+		<h1 class="text-4xl font-bold p-2">Rick and Martin</h1>
+	</div >
+	<div class ="border border-green-400 w-fit rounded-lg flex justify-center container mb-10">
+		<input type="text" class="input px-2 py-1 border border-none rounded-lg"/>
+		<button class = "searchButton bg-green-400 px-2 py-1 rounded-lg">Search</button>
+	</div>
+	<div class ="wrapper flex flex-wrap gap-4 justify-center items-center bg-slate-100 p-3 m-2">
+	</div>
+	<div>
+`;
+}
--- src/main.js
+++ src/main.js
@@ -1,18 +1,8 @@
 import "./style.css";
 import { GetCharacterHtml } from "./components/GetCharacterHtml";
+import { GetMainHtml } from "./components/GetMainHtml";
 
-document.querySelector("#app").innerHTML = /* html*/ `
-	<div class ="container">
-		<h1 class="text-4xl font-bold p-2">Rick and Martin</h1>
-	</div >
-	<div class ="border border-green-400 w-fit rounded-lg flex justify-center container mb-10">
-		<input type="text" class="input px-2 py-1 border border-none rounded-lg"/>
-		<button class = "searchButton bg-green-400 px-2 py-1 rounded-lg">Search</button>
-	</div>
-	<div class ="wrapper flex flex-wrap gap-4 justify-center items-center bg-slate-100 p-3 m-2">
-	</div>
-	<div>
-`;
+document.querySelector("#app").innerHTML = GetMainHtml();
 
 const wrapper = document.querySelector(".wrapper");
 let elementsList = [];
