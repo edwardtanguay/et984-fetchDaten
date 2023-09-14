0bf22c7e656d0ad78b91e653f6fe6db78fefb4a7
Edward Tanguay (windows) <edward@tanguay.info>
2023-09-14 13:48:17 +0200

    search works

--- src/components/GetWrapperHtml.js
+++ src/components/GetWrapperHtml.js
@@ -4,17 +4,14 @@ export const GetWrapperHtml = () => {
 	return /*html*/ `
 	<div class ="wrapper flex flex-wrap gap-4 justify-center items-center bg-slate-100 p-3 m-2"></div>
 	`;
-}; 
+};
 
 export const attachWrapperEvents = (characters) => {
 	const input = document.querySelector(".input");
 	input.addEventListener("input", (e) => {
 		const keyword = e.target.value.toLowerCase();
-		if (keyword !== "") {
-			const filteredCharacters = characters.filter((char) => char.name.toLowerCase().includes(keyword));
-			document.querySelector(".wrapper").innerHTML = GetCharacterListHtml(filteredCharacters);
-			return;
-		}
-
+		const filteredCharacters = characters.filter((char) => char.name.toLowerCase().includes(keyword));
+		document.querySelector(".wrapper").innerHTML = GetCharacterListHtml(filteredCharacters);
+		return;
 	});
-}
+};
